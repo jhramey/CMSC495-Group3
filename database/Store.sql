1d17d1a48cf373061e7b02c2c528e403274949cb
Jayson Ramey <jhramey1@gmail.com>
2020-04-30 12:18:32 -0400

    Added primary key to store table

--- database/Store.sql
+++ database/Store.sql
@@ -1,3 +1,5 @@
+-- Adminer 4.7.6 MySQL dump
+
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -6,12 +8,14 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 DROP TABLE IF EXISTS `store`;
 CREATE TABLE `store` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `color` varchar(255) NOT NULL,
-  `cost` varchar(255) NOT NULL,
-  `quan` varchar(255) NOT NULL,
-  `pic` varchar(1000) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `cost` float NOT NULL,
+  `quan` int(11) NOT NULL,
+  `pic` text NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+-- 2020-04-30 16:11:30
