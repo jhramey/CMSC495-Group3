99dd9d5a7fb0ee8d235f68b3709d31a9c4d11197
Jayson Ramey <jhramey1@gmail.com>
2020-05-04 13:54:27 -0400

    Added dynamic viewing of cart and wishlist using the tables.

--- public_html/JJRS_Account.php
+++ public_html/JJRS_Account.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+require_once "config.php";
+
+if(!$_SESSION["loggedin"]) {
+    header("location: JJRS_Login.php");
+} else {
+    $param_user_id = trim($_SESSION["id"]);
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,18 +18,28 @@ session_start();
     <body>
         <?php include('navbar.php'); ?>
         <h3>Your Wishlist:</h3>
+
+        <?php
+            // Get images from the database
+            $query = $link->query("SELECT store.pic, store.cost, store.id FROM wishlist, store WHERE wishlist.item_id=store.id AND wishlist.user_id='$param_user_id'");
+
+            if($query->num_rows > 0){
+                while($row = $query->fetch_assoc()){
+                    $imageURL = $row["pic"];
+                    $cost = $row["cost"];
+                    $item_id = $row["id"];
+        ?>
         <div id="item">
-            <h3>Black T-Shirt</h3>
-            <img id="item-image" src="images\black-tshirt.jpg">
-            <p>Price: $15</p>
-            <button id="wish-button" type="button">Remove From Wishlist</button>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <img id="item-image" src="<?php echo $imageURL; ?>" alt="" />
+                <p>Price: $<?php echo $cost;?></p>
+                <button id="wish-button" type="submit" value=<?php echo $item_id;?> name="removeFromWish">Remove From Wishlist</button>
+            </form>
         </div>
         <div id="space"></div>
-        <div id="item">
-            <h3>Black Hat</h3>
-            <img id="item-image" src="images\black-hat.jpg">
-            <p>Price: $10</p>
-            <button id="wish-button" type="button">Remove From Wishlist</button>
-        </div>
+        <?php }
+        }else{ ?>
+            <p>Wishlist is empty...</p>
+        <?php } ?>
     </body>
 </html>
--- public_html/JJRS_Cart.php
+++ public_html/JJRS_Cart.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+require_once "config.php";
+
+if(!$_SESSION["loggedin"]) {
+    header("location: JJRS_Login.php");
+} else {
+    $param_user_id = trim($_SESSION["id"]);
+}
 ?>
 
 <!DOCTYPE html>
@@ -10,23 +17,34 @@ session_start();
     </head>
     <body>
         <?php include('navbar.php'); ?>
+
         <div style="height:25px;"></div>
         <a href="JJRS_Checkout.php">
             <button id="checkout-button" type="button">Checkout</button>
         </a>
         <div style="height:25px;"></div>
+
+        <?php
+            // Get images from the database
+            $query = $link->query("SELECT store.pic, store.cost, store.id FROM cart, store WHERE cart.item_id=store.id AND cart.user_id='$param_user_id'");
+
+            if($query->num_rows > 0){
+                while($row = $query->fetch_assoc()){
+                    $imageURL = $row["pic"];
+                    $cost = $row["cost"];
+                    $item_id = $row["id"];
+        ?>
         <div id="item">
-            <h3>Black T-Shirt</h3>
-            <img id="item-image" src="images\black-tshirt.jpg">
-            <p>Price: $15</p>
-            <button id="cart-button" type="button">Remove From Cart</button>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <img id="item-image" src="<?php echo $imageURL; ?>" alt="" />
+                <p>Price: $<?php echo $cost;?></p>
+                <button id="cart-button" type="submit" value=<?php echo $item_id;?> name="removeFromCart">Remove From Cart</button>
+            </form>
         </div>
         <div id="space"></div>
-        <div id="item">
-            <h3>Black Hat</h3>
-            <img id="item-image" src="images\black-hat.jpg">
-            <p>Price: $10</p>
-            <button id="cart-button" type="button">Remove From Cart</button>
-        </div>
+        <?php }
+        }else{ ?>
+            <p>Cart is empty...</p>
+        <?php } ?>
     </body>
 </html>
