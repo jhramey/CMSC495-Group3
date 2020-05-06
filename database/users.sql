9091866704aebf7b27c47b2ceeee7bab20f3e52c
Rjritchie <62763005+Rjritchie@users.noreply.github.com>
2020-05-06 04:00:23 -0400

    Add files via upload

--- database/users.sql
+++ database/users.sql
@@ -12,12 +12,13 @@ CREATE TABLE `users` (
   `lastName` varchar(50) NOT NULL, 
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` date NOT NULL DEFAULT '0000-00-00',
+  `attempts` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `users` (`id`, `firstName`, `lastName`, `username`, `password`, `created_at`) VALUES
-(1, 'Jane', 'Doe', 'JaneDoe', '$2y$10$/4d4IIvNPg76dL1ybJs86.98ebvghtbiBQXBY5xfXr7htnWjXwTvO', '0000-00-00 00:00:00'),
-(2,	'admin', 'admin', 'admin', '$2y$10$0l0.u/HGwHPrcrPqvwmXruNLlDLgP0mTF0P20K4wf6EgeBWS5b5da', '0000-00-00 00:00:00');
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `username`, `password`, `attempts`, `created_at`) VALUES
+(1, 'Jane', 'Doe', 'JaneDoe', '$2y$10$/4d4IIvNPg76dL1ybJs86.98ebvghtbiBQXBY5xfXr7htnWjXwTvO', '0', '0000-00-00 00:00:00'),
+(2,	'admin', 'admin', 'admin', '$2y$10$0l0.u/HGwHPrcrPqvwmXruNLlDLgP0mTF0P20K4wf6EgeBWS5b5da', '0', '0000-00-00 00:00:00');
 
 -- 2020-04-11 18:08:11
